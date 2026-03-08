89e31bdda829e4b63bb75ed0ca96a1721b086364
kohtet107576-ai <kohtet107576@gmail.com>
2026-03-08 16:44:17 +0630

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // src folder ထဲက ဖိုင်တွေကိုပဲ သီးသန့်ဖတ်ခိုင်းရပါမယ်
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
