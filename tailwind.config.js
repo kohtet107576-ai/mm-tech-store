40a74c12e8b1dbdcfb5dcbfd6f46e6dcc0693aa1
kohtet107576-ai <kohtet107576@gmail.com>
2026-03-08 17:12:35 +0630

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./*.{js,ts,jsx,tsx}",        // Root folder ထဲက ဖိုင်တွေအတွက် (ဥပမာ: App.jsx, main.jsx)
+    "./src/**/*.{js,ts,jsx,tsx}", // src folder ထဲက ဖိုင်တွေအတွက်
+    "./components/**/*.{js,ts,jsx,tsx}" // components folder သုံးခဲ့ရင်
   ],
   theme: {
     extend: {},
