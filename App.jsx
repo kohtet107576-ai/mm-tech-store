abe18cef2c1a9b0fbd8818a1f09a7e99c6a50b93
kohtet107576-ai <kohtet107576@gmail.com>
2026-03-27 16:03:39 +0630

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -7,7 +7,7 @@ import { ShoppingBag, Gamepad2, Smartphone, ChevronRight, ArrowLeft, CheckCircle
 // --- (၁) CONFIGURATION ---
 const LOGO_URL = "https://drive.google.com/thumbnail?id=1Lh-nHgyLMSr3rBVe4OGnjEvEspuMokd6&sz=w1000"; 
 // အစ်ကို့ရဲ့ Unified Google Apps Script URL ပါ
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbySQdMbMW8TyvRwJPdI3njLEHg-aFib_tg2MU12LqIs-OWqvwEnMCJzi0-XBewB6ocN/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxZ0KASMPx7RpmEwgBGsecVzfNlC9SG1NlnxDo-9-q_FoA4kZVWHt7qM8tnbd4qsW5mBA/exec";
 const IMGBB_API_KEY = "88d3b49cfcf4fa4b1e77ce493aa3172a";
 const ADMIN_EMAILS = ["kohtet107576@gmail.com"]; 
 
