909002007d7f5a790c3641fd19505314d21770f3
kohtet107576-ai <kohtet107576@gmail.com>
2026-04-03 19:44:57 +0630

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ import { ShoppingBag, ArrowLeft, CheckCircle2, Loader2, User, ShieldCheck, LogOu
 
 // --- (၁) CONFIGURATION ---
 const LOGO_URL = "https://drive.google.com/thumbnail?id=1Lh-nHgyLMSr3rBVe4OGnjEvEspuMokd6&sz=w1000"; 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxPumkJsM5CscYoynMVgxh9DT7zwP2IHd1bHnU3Gqt_604MEhfLAbvKx7jJnv6pAyNd/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby538xir03xxxyEFn0Mb_XQC5EqOobCYBYsQWJRJaAHIJOip9tHg_sAeo8Ov8_fAola/exec";
 const IMGBB_API_KEY = "88d3b49cfcf4fa4b1e77ce493aa3172a";
 const ADMIN_EMAILS = ["kohtet107576@gmail.com"]; 
 
